b931af6faa657b38be1f1889cd2c5c4951a2d9db
locemarn <marcelon27@gmail.com>
2018-10-28 13:53:49 -0300

    addTransaction method

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -6,13 +6,13 @@ class Content extends Component {
   render() {
     return (
       <div>
-        <h1>Content</h1>
 
         <Form
           valueTransaction={this.props.valueTransaction}
           typeTransaction={this.props.typeTransaction}
           changeValue={this.props.changeValue}
           changeType={this.props.changeType}
+          addTransaction={this.props.addTransaction}
         />
       </div>
     )
