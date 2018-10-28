58ee7de975d77e9871ef0e6297e51c573e532bc0
locemarn <marcelon27@gmail.com>
2018-10-28 15:59:39 -0300

    fixing router bug and add icon in header

--- src/components/content/main/main.js
+++ src/components/content/main/main.js
@@ -7,28 +7,13 @@ class Main extends Component {
   render() {
     return (
       <div className="container buttons center">
-        <button 
-          className="btn-floating btn-large waves-effect waves-light teal" 
-          name="add"
-          component={props =>
-            <Link to={urls.add.path}
-            {...props}
-          />
-          }  
-        >
+        <a className="btn-floating btn-large waves-effect waves-light teal" href={urls.add.path}>
           <i className="material-icons">add</i>
-        </button>
-        <button
-          className="btn-floating btn-large waves-effect waves-light teal"
-          name="list"
-          component={props =>
-            <Link to={urls.list.path}
-            {...props}
-          />
-          }  
-        >
+        </a>
+        <a
+          className="btn-floating btn-large waves-effect waves-light teal" href={urls.list.path}>
           <i className="material-icons">list</i>
-        </button>
+        </a>
       </div>
     )
   }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
+import urls from '../../utils/urls'
 
 class Header extends Component {
   render() {
     return (
       <nav>
+        <div className="left">
+          <a className="btn-floating btn-large waves-effect waves-light" href={urls.home.path}>
+            <i className="material-icons teal">home</i>
+          </a>
+        </div>
         <div className="nav-wrapper teal">
           <a href="#!" className="brand-logo center">React Bank</a>
         </div>
